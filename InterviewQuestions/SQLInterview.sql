ccadd9a6a198b772c15ccebacd8877300ec3faef
aman-dutta <amandutta004@gmail.com>
2023-09-25 12:34:29 +0530

    Data Analyst Question Ankit Bansal 21st September 2023

--- InterviewQuestions/SQLInterview.sql
+++ InterviewQuestions/SQLInterview.sql
@@ -99,6 +99,85 @@ select juniors,
 from juniors_hired,
 		seniors_hired
 
+		/*
+		create table people
+		(id int primary key not null,
+		 name varchar(20),
+		 gender char(2));
+
+		 create table relations
+		(
+			c_id int,
+			p_id int,
+			FOREIGN KEY (c_id) REFERENCES people(id),
+			foreign key (p_id) references people(id)
+		);
+
+		insert into people (id, name, gender)
+		values
+			(107,'Days','F'),
+			(145,'Hawbaker','M'),
+			(155,'Hansel','F'),
+			(202,'Blackston','M'),
+			(227,'Criss','F'),
+			(278,'Keffer','M'),
+			(305,'Canty','M'),
+			(329,'Mozingo','M'),
+			(425,'Nolf','M'),
+			(534,'Waugh','M'),
+			(586,'Tong','M'),
+			(618,'Dimartino','M'),
+			(747,'Beane','M'),
+			(878,'Chatmon','F'),
+			(904,'Hansard','F');
+
+			insert into relations(c_id, p_id)
+		values
+			(145, 202),
+			(145, 107),
+			(278,305),
+			(278,155),
+			(329, 425),
+			(329,227),
+			(534,586),
+			(534,878),
+			(618,747),
+			(618,904);
+
+			select *
+			from people;
+
+			select *
+			from relations;
+
+			Question: Write a query that prints the names of a child and his parents in individual columns respectively in order of the
+			child.
+*/
+
+with cte as (
+	SELECT c_id,
+			name,
+			gender
+	FROM relations r
+	INNER JOIN people p 
+		on r.p_id = p.id
+),
+
+id_with_father_mother as (
+	SELECT c_id,
+		  max(case when gender = 'F' then name end) as 'Mother',
+		  max(case when gender = 'M' then name end) as 'Father'
+	FROM cte
+	GROUP BY c_id
+)
+
+SELECT p.name as Child,
+		Father,
+		Mother
+FROM id_with_father_mother a
+INNER JOIN people p
+	on a.c_id = p.id
+
 
 
 
